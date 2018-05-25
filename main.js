0d1be5969bc46bdaef0c7bf4d611d0b83bb4f028
robertmullin <bobmullin@outlook.com>
2018-05-24 20:50:47 -0400

    Final
    Final

---
+++ main.js
@@ -0,0 +1,44 @@
+// Navigation 
+
+function openNav() {
+    document.getElementById("navID").style.width = "100%";
+
+    document.getElementById("navToggle").style.display = "none";
+
+
+}
+
+function closeNav() {
+    document.getElementById("navID").style.width = "0";
+
+    document.getElementById("navToggle").style.display = "block";
+}
+
+// Quotes 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
